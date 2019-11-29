f8564d237db1205a99d138f953fc174ae7d847d6
BearSouL <mosakung@live.com>
2019-11-30 04:32:58 +0700

    Update middleware

--- js/sharenote/createShareNote.js
+++ js/sharenote/createShareNote.js
@@ -1,18 +1,16 @@
 import { httpPOST, userId } from '../callAPI.js';
 
 window.onload = function () {
-    document.getElementById("create-review-subject").onsubmit = function () {
-        let cover = document.getElementById("cover-share-note").value;
-        let title = document.getElementById("title-share-note").value;
-        let subjectName = document.getElementById("subject-name-share-note").value;
-        let section = document.getElementById("section-share-note").value;
-        let instructionName = document.getElementById("instruction-name-share-note").value;
-        let semeter = document.getElementById("semeter-share-note").value;
-        let tag = document.getElementById('tag-share-note').value;
-        let description = document.getElementById('description-share-note').value;
-        let pictureArray = document.getElementById('picture-array-share-note').value;
-
-        alert('1' + title + '2' + subjectName + '3' + section + '4' + semeter + '5' + tag + '6' + description);
+    document.getElementById("create-share-note").onsubmit = function () {
+        let cover = document.getElementById("cover-share-note-create").value;
+        let title = document.getElementById("title-share-note-create").value;
+        let subjectName = document.getElementById("subject-name-share-note-create").value;
+        let section = document.getElementById("section-share-note-create").value;
+        let instructionName = document.getElementById("instruction-name-share-note-create").value;
+        let semeter = document.getElementById("semeter-share-note-create").value;
+        let tag = document.getElementById('tag-share-note-create').value;
+        let description = document.getElementById('description-share-note-create').value;
+        let pictureArray = document.getElementById('picture-array-share-note-create').value;
 
         let tagArray = tag.split(/[#]/g).filter(n => n);
 
@@ -47,7 +45,7 @@ window.onload = function () {
 
         const body = JSON.parse(preBody);
 
-        httpPOST('http://localhost:3000/shareed/share-note', userId, body, (res) => {
+        httpPOST('http://localhost:3000/shareed/share-note', userId, body, (res, json) => {
             console.log(res);
         })
     }
