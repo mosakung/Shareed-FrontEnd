b5d4326d4aeeeb7d81700bf9d5aba7956cc9265d
Mint <mpataraphorn@gmail.com>
2019-11-30 19:17:43 +0700

    js view tutor

--- js/reviewtutor/viewReviewTutorId.js
+++ js/reviewtutor/viewReviewTutorId.js
@@ -5,23 +5,31 @@ const urlParams = new URLSearchParams(window.location.search);
 
 httpIdGET('http://localhost:3000/shareed/review-tutor', urlParams.get("postID"), userId, (state, json) => {
     document.getElementById('title').innerHTML = json.title;
-
+    document.getElementById('owner').innerHTML = json.Username;
+    document.getElementById('writeDown').innerHTML = json.dateTime;
+    document.getElementById('tutorName').innerHTML = json.tutorName;
+    document.getElementById('academy').innerHTML = json.academy;
+    document.getElementById('course').innerHTML = json.subjectTeacher;
+    document.getElementById('contact').innerHTML = json.contact;
+    document.getElementById('description').innerHTML = json.description;
+    document.getElementById('content').innerHTML = json.contect;
+    document.getElementById('numComment').innerHTML = json.countComment;
 });
 
 /*{
     "reviewTutorId": "c100000001",
-    "tutorName": "ty",
-    "academy": "po",
-    "subjectTeacher": "po",
-    "description": "popopo",
-    "contact": "popopopo",
-    "title": "study",
+    "tutorName": "ty",/
+    "academy": "po",/
+    "subjectTeacher": "po",/
+    "description": "popopo",/
+    "contact": "popopopo",/
+    "title": "study",/
     "userId": "100000001",
-    "dateTime": "2019-11-24T08:51:06.000Z",
-    "Username": "akarapon",
-    "contect": [],
+    "dateTime": "2019-11-24T08:51:06.000Z",/
+    "Username": "akarapon",/
+    "contect": [],/
     "tag": [],
     "comment": [],
-    "countComment": 0,
+    "countComment": 0,/
     "isOwner": true
 }*/ 
