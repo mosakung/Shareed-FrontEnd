12d967ea479e24806aa2b57d6bd6a301f71f9782
Mint <mpataraphorn@gmail.com>
2019-11-30 21:59:34 +0700

    .

--- js/reviewtutor/viewReviewTutorId.js
+++ js/reviewtutor/viewReviewTutorId.js
@@ -7,7 +7,7 @@ httpIdGET('http://localhost:3000/shareed/review-tutor', urlParams.get("postID"),
     document.getElementById('title').innerHTML = json.title;
     document.getElementById('owner').innerHTML = json.Username;
     document.getElementById('writeDown').innerHTML = json.dateTime;
-    document.getElementById('blah').innerHTML = json.dateTime;
+    document.getElementById('blah').innerHTML = json.cover;
     document.getElementById('tutorName').innerHTML = json.tutorName;
     document.getElementById('academy').innerHTML = json.academy;
     document.getElementById('course').innerHTML = json.subjectTeacher;
@@ -16,24 +16,24 @@ httpIdGET('http://localhost:3000/shareed/review-tutor', urlParams.get("postID"),
     document.getElementById('content').innerHTML = json.contect;
     document.getElementById('numComment').innerHTML = json.countComment;
 
-
 });
 
 
 /*{
-    "reviewTutorId": "c100000001",
-    "tutorName": "ty",/
-    "academy": "po",/
-    "subjectTeacher": "po",/
-    "description": "popopo",/
-    "contact": "popopopo",/
-    "title": "study",/
-    "userId": "100000001",
-    "dateTime": "2019-11-24T08:51:06.000Z",/
-    "Username": "akarapon",/
-    "contect": [],/
-    "tag": [],
-    "comment": [],
-    "countComment": 0,/
-    "isOwner": true
+    reviewTutorId: data[0].ReviewTutorID,
+           / tutorName: data[0].TutorName,
+           / academy: data[0].Academy,
+           / subjectTeacher: data[0].Subject_Teach,
+            /description: data[0].Des,
+           / contact: data[0].ContactLink,
+           / title: data[0].Title,
+           / cover: data[0].Cover,
+            userId: data[0].UserID,
+            /dateTime: data[0].Date_Time,
+           / Username: data[0].Username,
+           / contect: contentData,
+            tag: tagPost,
+            comment: comment,
+           / countComment: countComment[0].count,
+            isOwner: owner
 }*/ 
