0be9d163eee16844645cad842cce89f7bd94124d
BearSouL <mosakung@live.com>
2019-12-02 00:55:13 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- js/sharenote/editShareNote.js
+++ js/sharenote/editShareNote.js
@@ -1,12 +1,20 @@
-import { htppIdGET, httpPUT, userId } from '../callAPI.js';
+import { httpIdGET, httpPUT, userId } from '../callAPI.js';
 
 const urlParams = new URLSearchParams(window.location.search);
 
-const postId = urlParams.get("postId");
+const postId = urlParams.get("postID");
 
-htppIdGET('http://localhost:3000/shareed/share-note', postId, userId, (state, json) => {
+let originalCover = 1;
 
-    $('.fetch-form-share-note').append(fourmInput(json));
+httpIdGET('http://localhost:3000/shareed/share-note', postId, userId, (state, json) => {
+
+    if (!json.cover) $('.fetch-cover-share-note').append(formCover_noData(json));
+    else $('.fetch-cover-share-note').append(formCover_alreadyData(json));
+
+    $('.fetch-owner-username').append(formOwnerAndBy(json));
+
+    $('.fetch-form-input-all-js').append(formInputBeforeTag(json) + formTag(json));
+    $('.fetch-form-content-all-js').append(formContent(json));
 })
 
 function formCover_noData(json) {
@@ -16,29 +24,124 @@ function formCover_noData(json) {
         <div id="cover-upload"></div>
         <img id="blah" src="https://raw.githubusercontent.com/mosakung/Shareed-FrontEnd/develop/Photo/UploadPicture.png"
             alt="your image" class="center" />
-        <input id="cover-share-note-create" type="file" onchange="readURL(this);" class="center" />
     </div>
+
+    <input id="cover-share-note-create" type="file" onchange="readURL(this);" class="center" />
     `]
 }
 
-function formCover_alreadyData() {
+function formCover_alreadyData(json) {
     return [`
     <div class="center "
         style="background-color: rgba(255, 255, 255, 0.37);margin-top: 50px;width: 864px;height: 294px;">
-        <a style="cursor: grabbing;"><img id="remove-picture-cover"
-                    src="https://raw.githubusercontent.com/mosakung/Shareed-FrontEnd/develop/Photo/remove.png"
-                    onclick="cover_delete()" style="width: 50px; height: 50px; position: absolute;"></a>
-        <img id="blah" src="https://raw.githubusercontent.com/mosakung/Shareed-FrontEnd/develop/Photo/UploadPicture.png"
+        <div id="cover-upload"></div>
+        <a id="cover-original" style="cursor: grabbing;"><img id="remove-picture-cover"
+        src="https://raw.githubusercontent.com/mosakung/Shareed-FrontEnd/develop/Photo/remove.png"
+        onclick="cover_delete()" style="width: 50px; height: 50px; position: absolute;"></a>
+        <img id="blah" src="${json.cover}"
             alt="your image" class="center" />
-        <input id="cover-share-note-create" type="file" onchange="readURL(this);" class="center" />
+    </div>
+
+    <input id="cover-share-note-create" type="file" onchange="readURL(this);" class="center" />
+    `]
+}
+
+function formOwnerAndBy(json) {
+    return [`
+    <div class="box" style="height:50px; margin-top: 10px;">
+        <div class="col-sm-4">
+            Owner : ${json.Username}
+        </div>
+        <div class="col-sm-4" style="margin-left:250px;">
+            Write Down : ${json.dateTime.substring(0,10)}
+        </div>
+    </div>
+    `];
+}
+
+function formInputBeforeTag(json) {
+    return [`
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Title : </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="title-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);" placeholder="Title" value="${json.title}">
+        </div>
+    </div>
+    <hr>
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Subject Name:
+        </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="subject-name-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);" placeholder="e.g. CPExxx" value="${json.subjectName}">
+        </div>
+    </div>
+    <hr>
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Section : </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="section-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);" placeholder="e.g. 4" value="${json.section}">
+        </div>
+    </div>
+    <hr>
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Instruction Name :
+        </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="instruction-name-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);"
+                placeholder="Aj.xxxx xxxx" value="${json.instructorName}">
+        </div>
+    </div>
+    <hr>
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Semeter : </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="semeter-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);"
+                placeholder="e.g. 2nd semeter" value="${json.semeter}">
+        </div>
     </div>
     `]
 }
 
-function fourmInput(json) {
-    return [`
-    
-    `];
+function formTag(json) {
+    var body = ``;
+    body = body + `<hr>
+    <div class="form-group">
+        <label class="col-sm-3 control-label" style="font-size: 15px; text-align: left;">Tag : </label>
+        <div class="col-sm-9">
+            <input class="form-control" id="tag-share-note-create" type="text"
+                style="border: none; background-color:rgba(255, 255, 255, 0.37);"
+                placeholder="e.g. #CPExxx #Calculus" value="`;
+    json.tag.forEach(function (data, index) {
+        body = body + `#${data.TagDetail}`;
+    });
+    body = body + `">
+        </div>
+    </div>
+    <hr>`;
+    return body;
+}
+
+function formContent(json) {
+    var body = ``;
+    json.content.forEach(function (data, index) {
+        body = body + `
+        <div id="card-upload-${index}" class="row">
+            <div class="col-sm-12">
+                <div class="card mt-3" style="margin-top: 5%;">
+                    <a style="cursor: grabbing;"><img id="remove-picture-${index}"
+                            src="https://raw.githubusercontent.com/mosakung/Shareed-FrontEnd/develop/Photo/remove.png"
+                            onclick="remove_picture(${index})" style="width: 50px; height: 50px; position: absolute;"></a>
+                    <center><img id="blahs" src="${data.Picture}" class="center card-img-top" style="z-index: -1; max-width: 550px;"></center>
+                </div>
+            </div>
+        </div>`
+    })
+    return body;
 }
 
 window.onload = function () {
